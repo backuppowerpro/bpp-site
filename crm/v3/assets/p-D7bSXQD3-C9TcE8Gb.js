d7faf593aaf0df54af196ae951c6fbbe5be83685
Key Goodson <backuppowerpro.network@gmail.com>
2026-07-17 23:12:21 -0400

    Deploy from workspace ee629462

---
+++ crm/v3/assets/p-D7bSXQD3-C9TcE8Gb.js
@@ -0,0 +1,4 @@
+import{H as r,m as i,e as s,P as a,c as m}from"./ionic-app-C4fkhAin.js";import"./crm-shared-D7OMpb_X.js";import"./index-Bd0lTTHI.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!o)return;const t=i(o);t&&new Promise(n=>s(t,n)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
+//# sourceMappingURL=p-D7bSXQD3-C9TcE8Gb.js.map
---
+++ crm/v3/assets/web-CpRNj3CY.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-Bd0lTTHI.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-CpRNj3CY.js.map
