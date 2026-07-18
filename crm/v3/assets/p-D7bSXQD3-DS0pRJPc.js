dba914255a11e904937190e17ddcdf7a827e5921
Key Goodson <backuppowerpro.network@gmail.com>
2026-07-18 00:54:11 -0400

    Deploy from workspace ce16fbf6

--- crm/v3/assets/p-D7bSXQD3-DS0pRJPc.js
+++ crm/v3/assets/p-D7bSXQD3-DS0pRJPc.js
@@ -1,4 +1,4 @@
-import{H as r,m as i,e as s,P as a,c as m}from"./ionic-app-BlZyopGQ.js";import"./crm-shared-B8oJaLtD.js";import"./index-C-7KZ0rU.js";/*!
+import{H as r,m as i,e as s,P as a,c as m}from"./ionic-app-CfSduY4o.js";import"./crm-shared-B8oJaLtD.js";import"./index-PYy45r1r.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!o)return;const t=i(o);t&&new Promise(n=>s(t,n)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
-//# sourceMappingURL=p-D7bSXQD3-D_oozGyD.js.map
+//# sourceMappingURL=p-D7bSXQD3-DS0pRJPc.js.map
--- crm/v3/assets/web-LmvX5nT0.js
+++
@@ -1,2 +0,0 @@
-import{W as n}from"./index-C-7KZ0rU.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
-//# sourceMappingURL=web-LmvX5nT0.js.map
