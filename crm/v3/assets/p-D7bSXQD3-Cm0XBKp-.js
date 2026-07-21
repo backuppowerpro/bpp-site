c7f2f3cf4e7f0d7a17f1c8cb6a5c48a77525d10b
Key Goodson <backuppowerpro.network@gmail.com>
2026-07-21 18:50:04 -0400

    Deploy from workspace b6f5be24

--- crm/v3/assets/p-D7bSXQD3-Cm0XBKp-.js
+++ crm/v3/assets/p-D7bSXQD3-Cm0XBKp-.js
@@ -1,4 +1,4 @@
-import{H as r,m as i,e as s,P as a,c as m}from"./ionic-app-CcqyWyKW.js";import"./crm-shared-B8oJaLtD.js";import"./index-BqFrgoTE.js";/*!
+import{H as r,m as i,e as s,P as a,c as m}from"./ionic-app-Dy5ws-ll.js";import"./crm-shared-B8oJaLtD.js";import"./index-DdloYxeW.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=document.elementFromPoint(e.innerWidth/2,e.innerHeight/2);if(!o)return;const t=i(o);t&&new Promise(n=>s(t,n)).then(()=>{a(async()=>{t.style.setProperty("--overflow","hidden"),await m(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
-//# sourceMappingURL=p-D7bSXQD3-BcrCjH1W.js.map
+//# sourceMappingURL=p-D7bSXQD3-Cm0XBKp-.js.map
