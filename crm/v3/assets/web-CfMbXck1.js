b22a6aa0ea90e7a26d32f0050c77d7b62e1655f0
Key Goodson <backuppowerpro.network@gmail.com>
2026-07-21 02:16:41 -0400

    Deploy from workspace f1941a5e

--- crm/v3/assets/web-CfMbXck1.js
+++
@@ -1,2 +0,0 @@
-import{W as n}from"./index-D1txS4jX.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
-//# sourceMappingURL=web-CfMbXck1.js.map
---
+++ crm/v3/assets/web-DW9xi2YO.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-BCy3HlW1.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-DW9xi2YO.js.map
