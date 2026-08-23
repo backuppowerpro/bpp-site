3f1b88c42528056e623dab79ac28c7573c8b1bd5
Key Goodson <keygoodson@Keys-MacBook-Pro.local>
2026-08-22 20:33:10 -0400

    Revert "Restore verified Quote Walk release"
    This reverts commit 3de1cb056884cb3e2dd86823c695882c106388bc.

---
+++ walk-v2/no-js-bootstrap.js
@@ -0,0 +1,7 @@
+(function () {
+  var root = document.documentElement;
+  window.BPPQuoteWalkMarkReady = function () {
+    root.classList.remove('no-js');
+    root.classList.add('js');
+  };
+})();
